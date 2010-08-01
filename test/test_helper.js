dd09bccbb0219c3b6783697b0c659b87420ee1f7
Nick Stenning <nick@whiteink.com>
2010-08-01 15:14:46 +0100

    First steps. Convert test_archive.js and test_helper.js.

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,49 +1,113 @@
-require("test/unit")
-require("fileutils")
-require("logger")
-require((File.dirname("(string)") + "/../lib/git"))
-Test.Unit.TestCase = Class.extend({
-  set_file_paths: function () {
-    cwd = `pwd`.chomp()
-    if (File.directory?(File.join(cwd, "files"))) {
-      @test_dir = File.join(cwd, "files")
-    } else {
-      if (File.directory?(File.join(cwd, "..", "files"))) {
-        @test_dir = File.join(cwd, "..", "files")
-      } else {
-        if (File.directory?(File.join(cwd, "tests", "files"))) {
-          @test_dir = File.join(cwd, "tests", "files")
-        }
-      }
-    }
-    @wdir_dot = File.expand_path(File.join(@test_dir, "working"))
-    @wbare = File.expand_path(File.join(@test_dir, "working.git"))
-    @index = File.expand_path(File.join(@test_dir, "index"))
-    @wdir = create_temp_repo(@wdir_dot)
+require.paths.unshift(__dirname + '/../lib')
+
+var fs      = require('fs'),
+    assert  = require('assert'),
+    exec    = require('child_process').exec,
+    inspect = require('eyes').inspector()
+
+// This is a file designed to be loaded by each test, which
+// takes care of loading vows, assert, and any other libraries
+// needed by the tests.
+//
+// It places any properties in the "namespace" object below into
+// the global namespace of the tests.
+
+var extend = function(obj, props) {
+  Object.getOwnPropertyNames(props).forEach(function(prop){
+    var descriptor = Object.getOwnPropertyDescriptor(props, prop)
+    descriptor.enumerable = false
+    Object.defineProperty(obj, prop, descriptor)
+  })
+}
+
+extend(Object, {
+  merge: function (a, b) {
+    if (!b) return a
+    var keys = Object.keys(b)
+    for (var i = 0, len = keys.length; i < len; ++i)
+      a[keys[i]] = b[keys[i]]
+    return a
+  }
+})
+
+// Additional assertions
+
+assert.isFile = function isFile(fname, message) {
+  try {
+    stat = fs.statSync(fname)
+  } catch(e) {}
+  if (!stat || !stat.isFile()) assert.fail(undefined, fname, message, "is not a file", assert.isFile)
+}
+
+// Other utility functions
+
+function uid () {
+  var uid = ''
+  for (var n = 4; n; --n) {
+    uid += (Math.abs((Math.random() * 0xFFFFFFF) | 0)).toString(16)
+  }
+  return uid
+}
+
+function tempfile (name) {
+  return process.env.TMPDIR + (name || 'tempfile') + '-' + uid()
+}
+
+// node-git helpers
+
+var node_git = {
+  file_paths: { test_dir: __dirname
+              , wdir_dot: __dirname + '/files/working'
+              , wbare:    __dirname + '/files/working.git'
+              , index:    __dirname + '/files/index'
+              },
+
+  create_temp_repo: function create_temp_repo (clone_path, callback) {
+    tmp_path = tempfile('git_test')
+    fs.mkdirSync(tmp_path, 0755)
+    exec('cp -r ' + clone_path + ' ' + tmp_path, function () {
+      exec('mv ' + tmp_path + '/working/dot_git ' + tmp_path + '/working/.git', function () {
+        callback(tmp_path + '/working')
+        exec('rm -r ' + tmp_path)
+      })
+    })
   },
-  teardown: function () {
-    if (@tmp_path) FileUtils.rm_r(@tmp_path)
+
+  in_temp_dir: function (fun, remove_after) {
+    remove_after = typeof remove_after === 'undefined' ? true : remove_after
+    old_wd = process.cwd()
+
+    tmp_path = tempfile('git_test')
+    fs.mkdirSync(tmp_path, 0755)
+    process.chdir(tmp_path)
+    fun(tmp_path)
+    process.chdir(old_wd)
+
+    if (remove_after) exec('rm -r '+ tmp_path)
   },
-  create_temp_repo: function (clone_path) {
-    filename = (("git_test" + Time.now().to_i().to_s()) + rand(300).to_s().rjust(3, "0"))
-    @tmp_path = File.join("/tmp/", filename)
-    FileUtils.mkdir_p(@tmp_path)
-    FileUtils.cp_r(clone_path, @tmp_path)
-    tmp_path = File.join(@tmp_path, "working")
-    Dir.chdir(tmp_path) { FileUtils.mv("dot_git", ".git") }
-    tmp_path
+
+  get_wdir: function (callback) {
+    node_git.create_temp_repo(node_git.file_paths.wdir_dot, function (path) {
+      callback(path)
+    })
   },
-  in_temp_dir: function (remove_after = true) {
-    filename = (("git_test" + Time.now().to_i().to_s()) + rand(300).to_s().rjust(3, "0"))
-    tmp_path = File.join("/tmp/", filename)
-    FileUtils.mkdir(tmp_path)
-    Dir.chdir(tmp_path) { yield(tmp_path) }
-    if (remove_after) FileUtils.rm_r(tmp_path)
-  },
+
   new_file: function (name, contents) {
-    File.open(name, "w") { |f| f.puts(contents) }
+    fs.writeFileSync(name, contents)
   },
+
   append_file: function (name, contents) {
-    File.open(name, "a") { |f| f.puts(contents) }
-  },
-});
+    fs.writeFileSync(name, fs.readFileSync(name) + contents)
+  }
+}
+
+var namespace = { assert:   assert
+                , exec:     exec
+                , inspect:  inspect
+                , node_git: node_git
+                , sys:      require('sys')
+                , tempfile: tempfile
+                , vows:     require('vows')
+                }
+
+Object.merge(global, namespace)
--- test/units/test_archive.js
+++ test/units/test_archive.js
@@ -1,39 +1,70 @@
-require((File.dirname("(string)") + "/../test_helper"))
-TestArchive = Test.Unit.TestCase.extend({
-  setup: function () {
-    set_file_paths()
-    @git = Git.open(@wdir)
-  },
-  tempfile: function () {
-    Tempfile.new("archive-test").path()
-  },
-  test_archive: function () {
-    f = @git.archive("v2.6", tempfile())
-    assert(File.exists?(f))
-    f = @git.object("v2.6").archive(tempfile())
-    assert(File.exists?(f))
-    f = @git.object("v2.6").archive()
-    assert(File.exists?(f))
-    f = @git.object("v2.6").archive(nil, format: "tar")
-    assert(File.exists?(f))
-    lines = `cd /tmp; tar xvpf #{f}`.split("\n")
-    assert_equal("ex_dir/", lines[0])
-    assert_equal("example.txt", lines[2])
-    f = @git.object("v2.6").archive(tempfile(), format: "zip")
-    assert(File.file?(f))
-    f = @git.object("v2.6").archive(tempfile(), format: "tgz", prefix: "test/")
-    assert(File.exists?(f))
-    f = @git.object("v2.6").archive(tempfile(), format: "tar", prefix: "test/", path: "ex_dir/")
-    assert(File.exists?(f))
-    lines = `cd /tmp; tar xvpf #{f}`.split("\n")
-    assert_equal("test/", lines[0])
-    assert_equal("test/ex_dir/ex.txt", lines[2])
-    in_temp_dir {
-      c = Git.clone(@wbare, "new")
-      c.chdir {
-        f = @git.remote("origin").branch("master").archive(tempfile(), format: "tgz")
-        assert(File.exists?(f))
+require('../test_helper')
+// require('git')
+
+var suite = vows.describe('Archive')
+
+suite.addBatch({
+  'Archive': {
+    topic: function () {
+      node_git.get_wdir(function (path) {
+        this.callback(null, Git.open(path))
+      })
+    },
+
+    'should archive files': function (topic) {
+      f = topic.archive("v2.6", tempfile())
+      assert.isFile(f)
+      f = topic.object("v2.6").archive(tempfile())
+      assert.isFile(f)
+      f = topic.object("v2.6").archive()
+      assert.isFile(f)
+      f = topic.object("v2.6").archive(tempfile(), { format: "zip" })
+      assert.isFile(f)
+      f = topic.object("v2.6").archive(tempfile(), { format: "tgz", prefix: "test/" })
+      assert.isFile(f)
+    },
+
+    'tar': {
+      topic: function(parent) {
+        f = parent.object("v2.6").archive(nil, { format: "tar" })
+        exec('cd /tmp; tar xvpf ' + f, function (err, stdout, stderr) {
+          this.callback(stdout.split("\n"))
+        })
+      },
+
+      'should make a tarball': function (lines) {
+        assert.equal("ex_dir/", lines[0])
+        assert.equal("example.txt", lines[2])
+      }
+    },
+
+    'tar with prefix': {
+      topic: function(parent) {
+        f = parent.object("v2.6").archive(tempfile(), { format: "tar", prefix: "test/", path: "ex_dir/" })
+        exec('cd /tmp; tar xvpf ' + f, function (err, stdout, stderr) {
+          this.callback(stdout.split("\n"))
+        })
+      },
+
+      'should make a tarball with prefix': function (lines) {
+        assert.equal("test/", lines[0])
+        assert.equal("test/ex_dir/ex.txt", lines[2])
+      }
+    },
+
+    'new clone': {
+      topic: function (parent) {
+        node_git.in_temp_dir(function (path) { this.callback(null, parent) })
+      },
+
+      'should archive': function (topic) {
+        c = Git.clone(node_git.file_paths.wbare, "new")
+        // TODO: need to chdir into clone?
+        f = topic.remote("origin").branch("master").archive(tempfile(), { format: "tgz" })
+        assert.isFile(f)
       }
     }
-  },
-});
+  }
+})
+
+suite.export(module)
